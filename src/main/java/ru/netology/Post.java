c8966597643426aa840769a60854138ae5b29aec
mih sve <RMS4083@yandex.ru>
2023-07-20 22:51:12 +0300

    ШтшÐInitial commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+import ru.netology.qa.FormDate;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+}
+
+
